461b83f16771798d796406f8fb9d63d94ac83478
Ines Cari <ineslorenacari@hotmail.com>
2023-08-02 15:57:26 -0300

    cambio

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path= require("path");
-const port= process.env.PORT||3001;
+const port= process.env.PORT||8000;
 app.use(express.static(path.resolve(__dirname,"./public")))
 app.listen(port,()=> {
     console.log("Servidor corriendos");
