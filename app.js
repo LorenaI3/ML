27b4871c068b6bd93c50a2236f8f2bd9309bf254
Ines Cari <ineslorenacari@hotmail.com>
2023-08-07 02:47:41 -0300

    actualizado

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ app.listen(port,()=> {
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
+app.get("/views/register.html",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+});
+app.get("/views/login.html",(req,res)=>{
+    res.sendFile(path.join(__dirname, "//views/login.html"))
+});
